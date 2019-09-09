fa9dfbe93211c065f566dffc9559909df3af43ae
m11o <velonica2227@icloud.com>
2019-09-10 01:36:20 +0900

    feat: update

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,27 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'John',
+    };
+  }
+
+  handleChangeName(name) {
+    this.setState({ name });
+  }
+
   render() {
     return (
       <div>
-        <Greeting name="John" />
-        <Greeting name="Bob" />
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleChangeName(e.target.value)}
+        />
+        <button onClick={() => this.handleChangeName('Bob')}>I am Bob</button>
+        <Greeting name={this.state.name} />
       </div>
     );
   }
